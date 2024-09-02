3af914ced677dc7a120535a0a3d31fa1d15fc138
noaa <noaamaman325158@gmail.com>
2024-09-02 12:29:58 +0300

    refactor(boot.asm): switch to protected mode

--- boot.asm
+++ boot.asm
@@ -1,5 +1,8 @@
-ORG 0
+ORG 0x7c00
 BITS 16
+
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
 
 _start:
     jmp short start
@@ -8,12 +11,12 @@ _start:
 times 33 db 0
 
 start:
-    jmp 0x7C0:step2
+    jmp 0 :step2
 
 step2:
     cli                     ; Clear Interrupt Flag
 
-    mov ax, 0x7C0           ; Set up data segment
+    mov ax, 0x00          ; Set up data segment
     mov ds, ax
     mov es, ax
 
@@ -25,26 +28,58 @@ step2:
     
     
     jmp $  
+.load_protected:
+    cli
+    lgdt[gdt_descriptor]
+    mov eax, cr0
+    or eax, 0x1
+    mov cr0, eax
+    jmp CODE_SEG:load32
+
+; GDT
+gdt_start:
+
+gdt_null:
+    dd 0x00
+    dd 0x00
+
+; offset 0x8
+gdt_code: ; Code Segment should be point to this
+    dw 0xffff; Segment Limit 0-15 bits
+    dw 0; Base first 0-15 bits 
+    dw 0; Base 16-23 bits
+    db 0x9a; Access Byte
+    db 11001111b; High 4 bit flag and the low 4 bit flags
+    db 0; Base 24-31 bits
+
+; offset 0x10
+gdt_data: ; DS, SS, ES, FS, GS
+    dw 0xffff
+    dw 0
+    dw 0
+    db 0x92
+    db 11001111b
+    db 0
+gdt_end:
+
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1
+    dd gdt_start
 
 
-print:
-    mov bx, 0
-.loop:
-    lodsb 
-    cmp al, 0
-    je .done
-    call print_char
-    jmp .loop
-.done:
-    ret
-
-print_char:
-    mov ah, 0eh
-    int 0x10
-    ret
+[BITS 32]
+load32:
+    mov ax, DATA_SEG
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
     
+    mov ebp, 0x00200000
+    mov esp, ebp
 
-error_message db 'Failed to load sector', 0
+    jmp $
 
 times 510-($-$$) db 0
 dw 0xAA55
